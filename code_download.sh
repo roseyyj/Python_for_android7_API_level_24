351b66d874fd8cdfb141197fc530dc699549e1ab
roseyyj <roseyyj@users.noreply.github.com>
2018-04-26 17:04:59 -0700

    Update code_download.sh

--- code_download.sh
+++ code_download.sh
@@ -5,6 +5,9 @@ cd Python-2.7.14
 ##### 
 #Referring to Python documentation: https://bugs.python.org/issue3754
 #There is a bug for python embedded cross compile. 
-#Workaround is: set config.site
+#Workaround is: add config.site with following content. 
 # ac_cv_file__dev_ptmx=no
 # ac_cv_file__dev_ptc=no
+CONFIG_SITE=config.site ./configure --host=arm-linux-androideabi --prefix=/usr2/yijiangy/code_base/embedded_python/Python-2.7.14/arm64_python --build=x86_64-linux-gnu
+make 
+make install
