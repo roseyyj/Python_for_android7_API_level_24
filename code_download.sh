9cda42fb85f53e30ce6ffa7e0bb030752b565f54
roseyyj <roseyyj@users.noreply.github.com>
2018-04-26 17:01:20 -0700

    Update code_download.sh

--- code_download.sh
+++ code_download.sh
@@ -1,3 +1,10 @@
 wget https://www.python.org/ftp/python/2.7.14/Python-2.7.14.tgz
 tar -zxvf Python-2.7.14.tgz
 -->>> start from here. 
+cd Python-2.7.14 
+##### 
+#Referring to Python documentation: https://bugs.python.org/issue3754
+#There is a bug for python embedded cross compile. 
+#Workaround is: set config.site
+# ac_cv_file__dev_ptmx=no
+# ac_cv_file__dev_ptc=no
