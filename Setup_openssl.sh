9b357053843886b974d222ce256716714152b32a
roseyyj <roseyyj@users.noreply.github.com>
2018-05-04 13:50:30 -0700

    Update Setup_openssl.sh

--- Setup_openssl.sh
+++ Setup_openssl.sh
@@ -2,7 +2,7 @@ git clone https://github.com/openssl/openssl.git
 cd openssl
 export ANDROID_NDK=/usr2/yijiangy/code_base/ndk/android-ndk-r16b
 export PATH=$PATH:/usr2/yijiangy/code_base/my-android-toolchain/bin
-./Configure android-arm64 -D__ANDROID_API__=24 --openssldir=/usr/local/ssl/ --prefix=/usr/local/ssl -static 
+./Configure android-arm64 -D__ANDROID_API__=24 --openssldir=/usr/local/ssl/ --prefix=/usr/local/ssl
 make depend
 make -j32 all
-make install
+sudo -E make install
