6aba93eecc6ac832ae0146ed4d2d82539e9d725f
roseyyj <roseyyj@users.noreply.github.com>
2018-05-03 23:06:48 -0700

    Create Setup_openssl.sh
    Setup openssl for python.

---
+++ Setup_openssl.sh
@@ -0,0 +1,6 @@
+export ANDROID_NDK=/usr2/yijiangy/code_base/ndk/android-ndk-r16b
+export PATH=$PATH:/usr2/yijiangy/code_base/my-android-toolchain/bin
+./Configure android-arm64 -D__ANDROID_API__=24 --openssldir=/usr2/yijiangy/code_base/embedded_python/ssl/ --prefix=/usr2/yijiangy/code_base/embedded_python/ssl
+make depend
+make -j32 all
+make install
