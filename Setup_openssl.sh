88e00b0cc1e89eaa4f3dcdca4da740b080480a34
roseyyj <roseyyj@users.noreply.github.com>
2018-05-04 13:29:27 -0700

    Update Setup_openssl.sh

--- Setup_openssl.sh
+++ Setup_openssl.sh
@@ -1,3 +1,5 @@
+git clone https://github.com/openssl/openssl.git
+cd openssl
 export ANDROID_NDK=/usr2/yijiangy/code_base/ndk/android-ndk-r16b
 export PATH=$PATH:/usr2/yijiangy/code_base/my-android-toolchain/bin
 ./Configure android-arm64 -D__ANDROID_API__=24 --openssldir=/usr2/yijiangy/code_base/embedded_python/ssl/ --prefix=/usr2/yijiangy/code_base/embedded_python/ssl
